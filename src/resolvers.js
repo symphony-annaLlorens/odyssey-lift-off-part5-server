5e3681035945f93f2db98213d7c12f55f264a555
Anna Llorens <anna.llorens@symphony.com>
2022-10-16 17:33:40 +0200

    Deprecate length

--- src/resolvers.js
+++ src/resolvers.js
@@ -40,10 +40,15 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
+    
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    /* Track.author and Track.modules resolvers */
+    durationInSeconds:  ({ length }) => length,
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
